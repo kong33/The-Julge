64fac9fb0ae4f1dba5009e6c0bf4f030bf657664
jinhok96 <jinhok96a@gmail.com>
2024-04-16 18:16:18 +0900

    chore: Button 컴포넌트 파일 추가

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+// // import styles from '@/components/common/Button.module.scss';
+
+// type ButtonType = {
+//   children: React.ReactNode;
+//   isSolid: boolean;
+//   size: 'large' | 'medium' | 'small';
+//   isActive: boolean;
+//   className: string;
+// } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+// export default function Button({ children, isSolid, size, isActive, className }: ButtonType) {
+//   // const buttonClasses = classNames(styles.button, className, {});
+//   return <button type="button">{children}</button>;
+// }
