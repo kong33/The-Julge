65d09bdbd9aad740be1dafd0fbc8a51904c5572c
gaeun <kge0211114@gmail.com>
2024-04-16 17:09:36 +0900

    Fix: css 버그 수정 및 font scss 추가

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const nextConfig = {
   sassOptions: {
-   // prependData: `@import "src/styles/globals.scss";`,
+    // prependData: `@import "src/styles/globals.scss";`,
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    return config;
   },
   reactStrictMode: true,
   images: {
@@ -20,23 +29,5 @@ const nextConfig = {
       },
     ],
   },
-   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.scss$/, // SCSS 파일을 찾기 위한 정규식
-      use: [
-        'style-loader', // 스타일을 DOM에 삽입하는 데 사용됩니다.
-        'css-loader', // CSS 파일을 모듈로 변환합니다.
-        'sass-loader' // SCSS 파일을 CSS로 변환합니다.
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack', 'url-loader'],
-    });
-
-    return config;
-}
 };
 export default nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable */
-import '@/styles/globals.module.scss';
-import Filter from '@/components/feature/Filter/Filter';
-
 export default function Home() {
-  return <Filter />;
+  return <p>home</p>;
 }
