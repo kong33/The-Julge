91dc797cbe1b743ef0970cff8d8308b7b4caaf3c
jinhok96 <jinhok96a@gmail.com>
2024-04-16 20:12:38 +0900

    chore: Button 컴포넌트 작성 완료

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,38 @@
-// import React from 'react';
+import classNames from 'classnames';
+import React from 'react';
 
-// // import styles from '@/components/common/Button.module.scss';
+import styles from '@/components/common/Button.module.scss';
 
-// type ButtonType = {
-//   children: React.ReactNode;
-//   isSolid: boolean;
-//   size: 'large' | 'medium' | 'small';
-//   isActive: boolean;
-//   className: string;
-// } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className: string;
+  solid?: boolean;
+  size: 'large' | 'medium' | 'small';
+  active: boolean;
+  submit?: boolean; // 버튼 타입을 submit으로 지정
+}
 
-// export default function Button({ children, isSolid, size, isActive, className }: ButtonType) {
-//   // const buttonClasses = classNames(styles.button, className, {});
-//   return <button type="button">{children}</button>;
-// }
+/**
+ * Button 컴포넌트
+ * @param solid 버튼 스타일; boolean
+ * @param size 버튼 크기 required; large | medium | small
+ * @param active 버튼 상태 required; boolean
+ * @param submit 버튼 타입을 submit으로 지정; boolean
+ * @param className 버튼 커스텀 스타일; string
+ * @param onClick 버튼 클릭 이벤트 콜백 함수; function
+ */
+
+export default function Button({ children, className, solid, size, active, submit = false, onClick }: ButtonType) {
+  const buttonClasses = classNames(
+    styles.button,
+    styles[size],
+    solid && styles.solid,
+    !active && styles.inactive,
+    className
+  );
+
+  return (
+    <button className={buttonClasses} type={submit ? 'submit' : 'button'} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
