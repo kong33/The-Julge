4f69e25583e6dea9c4e30b059538e4bd2a07502a
gaeun <kge0211114@gmail.com>
2024-04-16 21:37:57 +0900

    Feat: Filter 기본 ui 제작

---
+++ pages/gaeun.tsx
@@ -0,0 +1,6 @@
+import Filter from '@/components/feature/Filter/Filter';
+import './index.module.scss';
+
+export default function gaeun() {
+  return <Filter />;
+}
--- src/components/feature/Filter/Filter.tsx
+++ src/components/feature/Filter/Filter.tsx
@@ -5,9 +5,38 @@ import { ReactComponent as CloseButton } from '@/public/svgs/closeButton.svg';
 export default function Filter() {
   return (
     <div className={styles.container}>
-      <h1>상세 필터</h1>
-      <p>위치</p>
-      <CloseButton />
+      <div>
+        <h1>상세 필터</h1>
+        <CloseButton />
+      </div>
+
+      <section>
+        <p>위치</p>
+        <div></div>
+        <section></section>
+      </section>
+
+      <hr />
+
+      <section>
+        <p>시작일</p>
+        <input />
+      </section>
+
+      <hr />
+
+      <section>
+        <p>금액</p>
+        <section>
+          <input />
+          <p>이상부터</p>
+        </section>
+      </section>
+
+      <section>
+        <button>초기화</button>
+        <button>적용하기</button>
+      </section>
     </div>
   );
 }
