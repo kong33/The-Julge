4c7d2b2910ff9fbd875ae949f157211bfeef50bb
jinhok96 <jinhok96a@gmail.com>
2024-04-17 16:14:31 +0900

    chore: QueryClient 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
