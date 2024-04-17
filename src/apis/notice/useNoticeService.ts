c8acbcb746166658ce7e4dba1d2a44efadc747c2
jinhok96 <jinhok96a@gmail.com>
2024-04-17 21:35:49 +0900

    chore: 반복되는 코드 공통화

--- src/apis/notice/useNoticeService.ts
+++ src/apis/notice/useNoticeService.ts
@@ -4,6 +4,15 @@ import queryOptions from '@/apis/notice/queries';
 
 import { GetNoticesParams, GetNotices, BaseNotices, BaseParams, PostNoticePayload, Item } from './notice.type';
 
+// res에서 data를 추출해서 반환하는 함수
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function selectData<T>(res: any) {
+  if (!res?.data) return res;
+  const { data: resData, ...rest } = res;
+  const data: T = resData?.data;
+  return { data, ...rest };
+}
+
 /**
  * 공고 목록을 조회합니다.
  * @param params \{
@@ -30,9 +39,7 @@ import { GetNoticesParams, GetNotices, BaseNotices, BaseParams, PostNoticePayloa
 // eslint-disable-next-line import/prefer-default-export
 export function useGetNotices(params: GetNoticesParams) {
   const res = useSuspenseQuery(queryOptions.getNotices(params));
-  const { data: resData, ...rest } = res;
-  const data: GetNotices = resData?.data;
-  return { data, ...rest };
+  return selectData<GetNotices>(res);
 }
 
 /**
@@ -54,9 +61,7 @@ export function useGetNotices(params: GetNoticesParams) {
 
 export function useGetNoticesByShopId(shopId: string, params: BaseParams) {
   const res = useSuspenseQuery(queryOptions.getNoticesByShopId(shopId, params));
-  const { data: resData, ...rest } = res;
-  const data: BaseNotices = resData?.data;
-  return { data, ...rest };
+  return selectData<BaseNotices>(res);
 }
 
 /**
@@ -76,9 +81,7 @@ export function useGetNoticesByShopId(shopId: string, params: BaseParams) {
 
 export function usePostNoticeByShopId(shopId: string, payload: PostNoticePayload) {
   const res = useMutation(queryOptions.postNoticeByShopId(shopId, payload));
-  const { data: resData, ...rest } = res;
-  const data: Item = resData?.data;
-  return { data, ...rest };
+  return selectData<Item>(res);
 }
 
 /**
@@ -93,9 +96,7 @@ export function usePostNoticeByShopId(shopId: string, payload: PostNoticePayload
 
 export function useGetNoticeByNoticeId(shopId: string, noticeId: string) {
   const res = useSuspenseQuery(queryOptions.getNoticeByNoticeId(shopId, noticeId));
-  const { data: resData, ...rest } = res;
-  const data: Item = resData?.data;
-  return { data, ...rest };
+  return selectData<Item>(res);
 }
 
 /**
@@ -110,7 +111,5 @@ export function useGetNoticeByNoticeId(shopId: string, noticeId: string) {
 
 export function usePutNoticeByNoticeId(shopId: string, noticeId: string, payload: PostNoticePayload) {
   const res = useMutation(queryOptions.putNoticeByNoticeId(shopId, noticeId, payload));
-  const { data: resData, ...rest } = res;
-  const data: Item = resData?.data;
-  return { data, ...rest };
+  return selectData<Item>(res);
 }
