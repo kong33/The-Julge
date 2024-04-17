c3764cf4a84c3d33e670e6fcdb955c7a76d3a64d
min3eo <110177217+min3eo@users.noreply.github.com>
2024-04-17 12:36:49 +0900

    Feat: SearchBar 추가

---
+++ src/components/common/gnb/gnbFeature/menu/searchbar/searchbar.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+
+import styles from './searchbar.module.scss';
+
+export default function Searchbar() {
+  const [searchInput, setSearchInput] = useState('');
+  const router = useRouter();
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handlePressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !e.nativeEvent.isComposing) {
+      const routePath = searchInput ? `/search?keyword=${searchInput}` : '/search';
+      router.push(routePath);
+      setSearchInput('');
+    }
+  };
+
+  return (
+    <input
+      value={searchInput}
+      onChange={handleChangeInput}
+      onKeyDown={handlePressEnter}
+      placeholder="가게 이름으로 찾아보세요"
+      className={styles.searchbar}
+    />
+  );
+}
