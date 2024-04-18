573b40ca53a1089214a4ceda5d7bfd58bb3cfac7
jinhok96 <jinhok96a@gmail.com>
2024-04-18 19:05:37 +0900

    chore: ErrorBoundary, Suspense를 AsyncBoundary로 대체

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.scss';
-import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
-import { Suspense } from 'react';
+
+import AsyncBoundary from '@/components/common/AsyncBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -10,12 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
   // ErrorBoundary: 자바스크립트 오류 처리; 비동기 에러의 경우 컴포넌트에서 throw error를 통해 자바스크립트 에러를 발생시킬 수 있다.
   // Suspense: 비동기 로딩 처리
   return (
-    <ErrorBoundary errorComponent={undefined}>
-      <Suspense fallback={null}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </Suspense>
-    </ErrorBoundary>
+    <AsyncBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </AsyncBoundary>
   );
 }
