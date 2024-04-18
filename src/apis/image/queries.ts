71747a530509b49bba2498e8c1b2cf1889451b7b
jinhok96 <jinhok96a@gmail.com>
2024-04-18 18:19:46 +0900

    chore: usePutImage, useGetImage 추가

--- src/apis/image/queries.ts
+++ src/apis/image/queries.ts
@@ -2,13 +2,23 @@ import ImageService from '@/apis/image/Image.service';
 import { PostImagePayload } from '@/apis/image/image.type';
 
 const queryKeys = {
-  postImage: (name: string) => ['postImage', name] as const
+  postImage: (name: string) => ['postImage', name] as const,
+  putImage: (file: File, presignedUrl: string) => ['putImage', { file, presignedUrl }] as const,
+  getImage: (presignedUrl: string) => ['getImage', presignedUrl] as const
 };
 
 const queryOptions = {
   postImage: (name: string) => ({
     mutationKey: queryKeys.postImage(name),
     mutationFn: (postData: PostImagePayload) => ImageService.postImage(postData)
+  }),
+  putImage: (file: File, presignedUrl: string) => ({
+    mutationKey: queryKeys.putImage(file, presignedUrl),
+    mutationFn: (putFile: File) => ImageService.putImage(putFile, presignedUrl)
+  }),
+  getImage: (presignedUrl: string) => ({
+    queryKey: queryKeys.getImage(presignedUrl),
+    queryFn: () => ImageService.getImage(presignedUrl)
   })
 };
 
--- src/apis/image/useImageService.ts
+++ src/apis/image/useImageService.ts
@@ -1,8 +1,8 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 
 import { PostImageRes, PostImagePayload } from '@/apis/image/image.type';
 import queryOptions from '@/apis/image/queries';
-import selectData from '@/apis/utils';
+import { selectData } from '@/apis/utils';
 
 /**
  * Presigned URL을 생성합니다.
@@ -23,8 +23,26 @@ import selectData from '@/apis/utils';
 }
  */
 
-// eslint-disable-next-line import/prefer-default-export
 export function usePostImage({ name }: PostImagePayload) {
   const res = useMutation(queryOptions.postImage(name));
   return selectData<PostImageRes>(res);
+}
+
+/**
+ * S3로 이미지를 업로드합니다.
+ * @param file required; File
+ * @param presignedUrl required; string
+ * @returns  */
+
+export function usePutImage(file: File, presignedUrl: string) {
+  return useMutation(queryOptions.putImage(file, presignedUrl));
+}
+
+/**
+ * Presigned URL을 조회합니다.
+ * @param presignedUrl required; string
+ * @returns  */
+
+export function useGetImage(presignedUrl: string) {
+  return useSuspenseQuery(queryOptions.getImage(presignedUrl));
 }
