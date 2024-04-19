93ee54790c7cd1332da46336e3cdc3b6e2e6f1cc
jinhok96 <jinhok96a@gmail.com>
2024-04-19 23:23:05 +0900

    chore: type=number일 경우 value 포맷팅

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '@/components/common/Input/Input.module.scss';
 
@@ -8,11 +8,31 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className = '', invalid = false, ...rest }: InputProps, ref) => {
-    // const {  } = props;
+// 숫자에 콤마를 추가하는 함수
+const formatNumber = (value: string) => {
+  // 숫자 이외의 문자 제거, 선행하는 0 제거, 단 숫자가 0이거나 비어 있지 않은 경우를 제외
+  const cleanNum = value.replace(/\D/g, '').replace(/^0+/, '') || '0';
+  // 세 자리마다 콤마를 추가
+  return cleanNum.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = '', invalid = false, type, ...rest }: InputProps, ref) => {
     const inputClasses = classNames(styles.defaultInput, invalid && styles.invalid, className);
+
+    const [numValue, setNumValue] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      const formattedNumber = formatNumber(value);
+      setNumValue(formattedNumber);
+    };
+
+    if (type === 'number') {
+      return (
+        <input className={inputClasses} ref={ref} type="text" value={numValue} onChange={handleChange} {...rest} />
+      );
+    }
 
     return <input className={inputClasses} ref={ref} {...rest} />;
   }
