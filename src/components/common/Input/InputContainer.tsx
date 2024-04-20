3ede26fab600805b13786a9027b529fda71490ba
jinhok96 <jinhok96a@gmail.com>
2024-04-21 02:22:07 +0900

    chore: input 컴포넌트 공통 레이아웃 InputContainer 컴포넌트 추가

---
+++ src/components/common/Input/InputContainer.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import React from 'react';
+
+import ErrorMessage from '@/components/common/Input/ErrorMessage';
+import styles from '@/components/common/Input/InputContainer.module.scss';
+import Label from '@/components/common/Input/Label';
+
+interface InputContainerProps {
+  className?: string;
+  children: React.ReactNode;
+  label?: string;
+  required?: boolean;
+  errorMessage?: string | undefined | null;
+}
+
+export default function InputContainer({
+  className = '',
+  label = '',
+  required = false,
+  errorMessage = '',
+  children,
+}: InputContainerProps) {
+  const inputFormContainer = classNames(styles.inputFormContainer, className);
+
+  return (
+    <div className={inputFormContainer}>
+      <Label htmlFor={label} required={required}>
+        {label}
+      </Label>
+      {children}
+      <ErrorMessage>{errorMessage}</ErrorMessage>
+    </div>
+  );
+}
