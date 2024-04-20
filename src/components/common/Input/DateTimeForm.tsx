fce7bc06b710592bf692d0d7a866ddf5fbcd1904
jinhok96 <jinhok96a@gmail.com>
2024-04-20 23:40:34 +0900

    chore: DateTimeForm과 react-hook-form 연동 강화, jsdoc 업데이트

--- src/components/common/Input/DateTimeForm.tsx
+++ src/components/common/Input/DateTimeForm.tsx
@@ -1,7 +1,7 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import classNames from 'classnames';
 import ko from 'date-fns/locale/ko';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 
 import styles from '@/components/common/Input/DateTimeForm.module.scss';
@@ -11,20 +11,31 @@ registerLocale('ko', ko); // 한국어 로케일
 
 interface DateTimeFormProps {
   className?: string;
-  value: Date;
+  value: string;
   // eslint-disable-next-line no-unused-vars
   onChange: (date: Date) => void;
+  onBlur?: () => void;
 }
 
-export default function DateTimeForm({ className, value, onChange }: DateTimeFormProps) {
+/**
+ * 날짜, 시간을 입력하는 컴포넌트입니다. react-datepicker를 사용합니다.
+ * react-hook-form의 Controller에 대응됩니다.
+ * @param className string; InputForm 커스텀 스타일; 컨테이너의 스타일을 주입할 수 있습니다.
+ * @param value string; input의 value; ISO8601 형식; dateStr = new Date().toISOString();
+ * @param onChange (date: Date) => void;
+ * @returns
+ */
+const DateTimeForm = forwardRef(({ className, value, onChange, onBlur }: DateTimeFormProps, ref) => {
   const datePickerClass = classNames(styles.datePicker, className);
 
   return (
     <div className={datePickerClass}>
       <GlobalStyle />
       <DatePicker
-        selected={value}
+        selected={new Date(value)}
         onChange={onChange}
+        onBlur={onBlur}
+        ref={ref as React.Ref<DatePicker>}
         showTimeSelect
         timeFormat="HH:mm"
         timeIntervals={30}
@@ -34,4 +45,6 @@ export default function DateTimeForm({ className, value, onChange }: DateTimeFor
       />
     </div>
   );
-}
+});
+
+export default DateTimeForm;
