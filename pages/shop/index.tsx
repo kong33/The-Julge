0e95ed72665fc7c691a081fd5fbd5bc98786c293
jinhok96 <jinhok96a@gmail.com>
2024-04-22 22:07:50 +0900

    chore: /shop 페이지 모바일 작성

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,3 +1,26 @@
+import { useRouter } from 'next/router';
+
+import Button from '@/components/common/Button';
+import styles from '@/pages/shop/index.module.scss';
+
 export default function ShopPage() {
-  return <div>/shop</div>;
+  const router = useRouter();
+
+  const handleClickButton = () => {
+    router.push('/shop/register');
+  };
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <h1 className={styles.title}>내 가게</h1>
+        <article className={styles.article}>
+          <p className={styles.description}>내 가게를 소개하고 공고도 등록해 보세요.</p>
+          <Button onClick={handleClickButton} size="medium" active solid>
+            가게 등록하기
+          </Button>
+        </article>
+      </section>
+    </main>
+  );
 }
