dba448d1c1022285159a57daf1bbc990583e77d2
jinhok96 <jinhok96a@gmail.com>
2024-04-23 19:17:28 +0900

    chore: shop 페이지 리다이렉트 처리

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,13 +1,40 @@
-import router from 'next/router';
+import { jwtDecode } from 'jwt-decode';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import { useGetUser } from '@/apis/user/useUserService';
 import Button from '@/components/common/Button';
 import MainLayout from '@/layouts/MainLayout';
 import { pageList } from '@/libs/constants';
 import styles from '@/pages/shop/index.module.scss';
 
+// 토큰; 나중에 쿠키로 대체
+let token: string = '';
+let userId: string = '';
+if (typeof window !== 'undefined') {
+  token = localStorage.getItem('token') ?? '';
+  if (token) userId = jwtDecode<{ userId: string }>(token).userId ?? '';
+}
+
 // (shopId X) 사장님 가게 상세
 export default function ShopPage() {
+  const router = useRouter();
+
+  // 토큰이 없으면 로그인 페이지로 리다이렉트
+  // 쿠키로 대체되면 tpyeof window === 'undefined' 제거
+  if (typeof window !== 'undefined') {
+    if (!token) {
+      router.replace(pageList.login());
+    }
+  }
+
+  // shopId가 있으면 /shop/[shopId] 페이지로 리다이렉트
+  const { data } = useGetUser(userId);
+  const shopId = data?.item?.shop?.item?.id ?? '';
+  if (shopId) {
+    router.replace(pageList.shopDetail(shopId));
+  }
+
   const handleClick = {
     toShopRegisterPage: () => router.push(pageList.shopRegister())
   };
