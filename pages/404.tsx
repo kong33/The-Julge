9e0ae1d561f9b65f45f7cac3ca2e5dd5d7cea9ae
jinhok96 <jinhok96a@gmail.com>
2024-04-23 17:49:29 +0900

    chore: 404페이지 추가

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+
+import Button from '@/components/common/Button';
+import { pageList } from '@/libs/constants';
+
+export default function Page404() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.replace(pageList.shop());
+  };
+
+  return (
+    <Button active size="large" onClick={handleClick}>
+      404/shop 페이지로 돌아가기
+    </Button>
+  );
+}
