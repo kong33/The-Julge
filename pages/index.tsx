6cca8803c402ec338aae1a709a4bb84af9c4b72f
naeun14 <mimimi303@naver.com>
2024-04-23 16:31:52 +0900

    fix: main style 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,5 @@
-import Head from 'next/head';
-
 import styles from '@/pages/index.module.scss';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>The-Julge</title>
-      </Head>
-      <p className={styles.text}>home</p>
-    </>
-  );
+  return <p className={styles.text}>home</p>;
 }
