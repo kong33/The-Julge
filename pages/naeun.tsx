8acf6af2c0ac1c2fe8a6b769a361d2dd99235023
naeun14 <mimimi303@naver.com>
2024-04-23 16:22:39 +0900

    fix: 페이지네이션 훅 수정

--- pages/naeun.tsx
+++ pages/naeun.tsx
@@ -1,54 +1,47 @@
-import { ReactElement, useState } from 'react';
+import { useGetNoticeList } from '@/apis/notice/useNoticeService';
+import Pagination from '@/components/feature/pagination/pagination';
+import usePagination from '@/hooks/usePagination';
 
-import Modal from '@/components/feature/Modal/Modal';
-import ModalGroup from '@/components/feature/Modal/ModalGroup';
+const ITEMS_PER_PAGE = 6; // 페이지 당 아이템 수
+type Shop = {
+  id: string;
+  name: string;
+  category: string;
+  address1: string;
+  address2: string;
+  description: string;
+  imageUrl: string;
+  originalHourlyPay: number;
+};
+
+type Item = {
+  id: string;
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  description: string;
+  closed: boolean;
+  shop: {
+    item: Shop;
+    href: string;
+  };
+};
 
 function Naeun() {
-  const handleClick = () => {
-    console.log(`모달내부에 신청취소 혹은 승인 취소 버튼 클릭함!!!`);
-  };
-  const a = true;
-  // 조건에 따라 다른 모달이 떠야할 때
-  const [openModal, setOpenModal] = useState<ReactElement | null>(null);
-
-  const modalList = {
-    modal1: <Modal.Error>에러가 뜹니다!</Modal.Error>,
-    modal2: <Modal.Check>2</Modal.Check>,
-    modal3: <Modal.Select buttonClick={handleClick}>3</Modal.Select>,
-    modal4: <Modal.Select buttonClick={handleClick}>4</Modal.Select>
-  };
-
-  const onClick1 = () => {
-    if (a) {
-      setOpenModal(modalList.modal1);
-    }
-    if (!a) {
-      setOpenModal(modalList.modal2);
-    }
-  };
-
-  // 서로 다른 위치의 버튼 모두 모달이 필요할 때 root를 선언해야함!!
+  // const { data } = useGetNoticeList({ limit: ITEMS_PER_PAGE });
+  // console.log(data);
+  const { currentPage, totalPages, onPageChange, currentItems } = usePagination(useGetNoticeList, ITEMS_PER_PAGE);
+  console.log(currentPage, totalPages, onPageChange, currentItems);
   return (
-    <div>
-      {/* 모달선언 */}
-      <ModalGroup.Trigger>
-        <button type="button" onClick={onClick1}>
-          버튼1
-        </button>
-      </ModalGroup.Trigger>
-      <ModalGroup.Content>{openModal}</ModalGroup.Content>
-
-      <ModalGroup.Root>
-        <ModalGroup.Trigger>
-          <button type="button" onClick={onClick1}>
-            버튼2
-          </button>
-        </ModalGroup.Trigger>
-        <ModalGroup.Content>
-          <Modal.Select buttonClick={handleClick}>3</Modal.Select>
-        </ModalGroup.Content>
-      </ModalGroup.Root>
-    </div>
+    <>
+      <h1>Items</h1>
+      <ul>
+        {currentItems.map((item: Item, index: number) => (
+          <li>{index}</li>
+        ))}
+      </ul>
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
+    </>
   );
 }
 
--- src/libs/hooks/usePagination.ts
+++ src/libs/hooks/usePagination.ts
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react';
 
-export default function usePagination(totalItems: number, itemsPerPage: number) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function usePagination(apifunction: any, itemsPerPage: number) {
   const [currentPage, setCurrentPage] = useState<number>(1); // 현재 페이지 위치
+  const { data } = apifunction({ limit: itemsPerPage });
   // 전체 페이지 = (전체 카드 개수 / 한 페이지에 보여주는 카드 개수)의 올림수
-  const totalPages: number = Math.ceil(totalItems / itemsPerPage);
+  const totalPages: number = Math.ceil(data.count / itemsPerPage);
 
+  const totalItems = data.count;
   // 페이지 위치 변경
   const onPageChange = (page: number): void => {
     setCurrentPage(page);
   };
 
-  const startIndex: number = (currentPage - 1) * itemsPerPage;
-  const endIndex: number = startIndex + itemsPerPage;
   useEffect(() => {
     setCurrentPage(1);
   }, [totalItems]); // totalItems가 변경될 때마다 currentPage를 1로 초기화
@@ -20,7 +21,6 @@ export default function usePagination(totalItems: number, itemsPerPage: number) 
     currentPage,
     totalPages,
     onPageChange,
-    startIndex,
-    endIndex
+    currentItems: data.items
   };
 }
