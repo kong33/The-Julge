b75a7031aed0a69c552e6e4b738b4becb67d3e81
gaeun <kge0211114@gmail.com>
2024-04-23 16:36:45 +0900

    Feat: loginPage 생성

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,21 @@
-// import { usePostAuthentication } from "@/apis/authentication/useAuthenticationService"
+// import { usePostAuthentication } from "@/apis/authentication/useAuthenticationService";
+
 // export default function login() {
-// //     payload : {
-// //     email: string;
-// //     password: String;
-// // }//로그인 input에 들어온 email, password 를 payload 로관리
+//     //     payload : {
+//     //     email: string;
+//     //     password: String;
+//     // }//로그인 input에 들어온 email, password 를 payload 로관리
 //     const response = usePostAuthentication(payload);
 //     //response 생김새
-// //       item: {
-// //   token: string;
-// //   user: User;
-// // };
-// //   links: Array\<Link>;
-// // }
-// //if(response.ok)
-// //cookie 에 세팅
+//     //       item: {
+//     //   token: string;
+//     //   user: User;
+//     // };
+//     //   links: Array\<Link>;
+//     // }
+//     //if(response.ok)
+//     //cookie 에 세팅
 //     return (
 
 //     )
+// }
--- src/components/feature/AuthForm/AuthForm.tsx
+++ src/components/feature/AuthForm/AuthForm.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState, MouseEvent } from 'react';
 
 import Button from '@/components/common/Button';
+import InputForm from '@/components/common/Input/InputForm/InputForm';
 import { ReactComponent as Logo } from '@/public/svgs/Logo.svg';
 import { ReactComponent as ButtonCheck } from '@/public/svgs/buttonCheck.svg';
 import { ReactComponent as ButtonUnCheck } from '@/public/svgs/buttonUnCheck.svg';
@@ -19,13 +20,19 @@ const status = {
     formLabel: ['이메일', '비밀번호'],
     buttonText: '로그인 하기',
     footerText: '회원이 아니신가요?',
-    footerLink: '회원가입하기'
+    footerLink: '회원가입하기',
+    errorMessage: ['이메일 형식으로 작성해 주세요.', '8자 이상으로 입력해주시고, 특수문자를 1개 이상 포함해주세요.']
   },
   signin: {
     formLabel: ['이메일', '비밀번호', '비밀번호 확인'],
     buttonText: '가입하기',
     footerText: '이미 가입하셨나요?',
-    footerLink: '로그인하기'
+    footerLink: '로그인하기',
+    errorMessage: [
+      '이메일 형식으로 작성해 주세요.',
+      '8자 이상으로 입력해주시고, 특수문자를 1개 이상 포함해주세요.',
+      '비밀번호를 확인해주세요.'
+    ]
   }
 };
 
@@ -50,10 +57,11 @@ export default function AuthForm({ type }: AuthFormProps) {
     <form>
       <Logo width="248" height="44" />
       <div>
-        <input />
-        <input />
+        {status[type].formLabel.map((item, index) => (
+          <InputForm label={item} errorMessage={status[type].errorMessage[index]} type="string" />
+        ))}
         <Button size="large" solid submit active>
-          로그인 하기
+          {status[type].buttonText}
         </Button>
       </div>
       <div>
@@ -61,7 +69,7 @@ export default function AuthForm({ type }: AuthFormProps) {
         <Link href="/">{status[type].footerLink}</Link>
         {type === 'signin' && (
           <>
-            <input />
+            <InputForm label={status[type].formLabel[2]} errorMessage={status[type].errorMessage[2]} type="string" />
             <Button size="large" submit active id="알바님" onClick={handleClicked}>
               {isClicked ? <ButtonCheck /> : <ButtonUnCheck />}알바님
             </Button>
