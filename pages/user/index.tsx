93954b6bc3b11f173a416d3541ed40119849d6ae
min3eo <110177217+min3eo@users.noreply.github.com>
2024-04-25 20:00:27 +0900

    TJ-40 <유저 상세 페이지에 데이터 연결하기>

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -2,78 +2,93 @@ import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react';
 
+import UserService from '@/apis/user/User.service';
 import ApplyDetail from '@/components/layout/UserPage/ApplyDetail';
 import ProfileCard from '@/components/layout/UserPage/ProfileCard';
+import { pageList } from '@/libs/constants';
 
 import styles from './index.module.scss';
 
-type UserProfile = {
-  isProfile: boolean;
-  name: string;
-  phone: string;
-  address: string;
-  bio: string;
+type UserData = {
+  item: {
+    name: string;
+    phone: string;
+    address: string;
+    bio: string;
+  };
 };
 
-type Notice = {
+type Applications = {
   id: string;
   title: string;
   description: string;
-  createdAt: Date;
-};
-
-type NoticeListData = {
-  items: Notice[];
+  createdAt: string;
 };
 
 type Props = {
-  userData: UserProfile;
-  noticeListData: NoticeListData;
+  userId: string;
+  userData: UserData;
+  ApplicationData: Applications[];
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  // ()안에)context: GetServerSidePropsContext
-  const userData: UserProfile = {
-    isProfile: false,
-    name: '김성운',
-    phone: '010-1234-4321',
-    address: '서울시 도봉구',
-    bio: '열심히 하겠습니다'
-  };
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const { req } = context;
+  const { cookies } = req;
+  const { userId } = cookies;
 
-  const noticeListData: NoticeListData = {
-    items: []
-  };
-  return { props: { userData, noticeListData } };
+  if (!userId) {
+    return {
+      redirect: {
+        destination: pageList.home(),
+        permanent: false
+      }
+    };
+  }
+
+  const { data: userData } = await UserService.getUser(userId);
+  if (!userData) {
+    return {
+      notFound: true
+    };
+  }
+  const ApplicationData = [
+    {
+      id: 'qwer',
+      title: 'Application Title',
+      description: 'Description of the application',
+      createdAt: '2024-04-25T09:00:00'
+    }
+  ];
+
+  return { props: { userId, userData, ApplicationData } };
 };
 
-export default function UserDetailPage({ userData, noticeListData }: Props) {
+export default function UserDetailPage({ userId, userData, ApplicationData }: Props) {
   const router = useRouter();
   const onClickEdit = () => {
-    router.push('/user/edit');
+    router.push(pageList.userEdit());
   };
   const onClickList = () => {
-    router.push('/');
+    router.push(pageList.home());
   };
+  console.log('유저아이디', userId);
+  console.log('유저 공고', userData.item);
+
+  const isRegisterd = !!(userData.item && userData.item.name);
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <section className={`${styles.profileSection} ${!userData.isProfile ? styles.columnDirection : ''}`}>
+        <section className={`${styles.profileSection} ${!isRegisterd ? styles.columnDirection : ''}`}>
           <h1>내 프로필</h1>
-          <ProfileCard
-            isProfile={userData.isProfile}
-            name={userData.name}
-            phone={userData.phone}
-            address={userData.address}
-            bio={userData.bio}
-            onClickEdit={onClickEdit}
-          />
+          <ProfileCard isRegisterd={isRegisterd} onClickEdit={onClickEdit} {...userData.item} />
         </section>
-        <section className={styles.listSection}>
-          <h1>신청 내역</h1>
-          <ApplyDetail notices={noticeListData.items} onClickList={onClickList} />
-        </section>
+        {isRegisterd && (
+          <section className={styles.listSection}>
+            <h1>신청 내역</h1>
+            <ApplyDetail ApplicationData={ApplicationData} onClickList={onClickList} />
+          </section>
+        )}
       </div>
     </div>
   );
--- src/libs/constants/index.ts
+++ src/libs/constants/index.ts
@@ -1,4 +1,4 @@
-import { Address as AddressType } from '@/apis/common.type';
+import { Address as AddressType, Category } from '@/apis/common.type';
 
 export const addressList: AddressType[] = [
   '서울시 종로구',
@@ -28,5 +28,42 @@ export const addressList: AddressType[] = [
   '서울시 강동구'
 ];
 
+export const categoryList: Category[] = ['한식', '중식', '일식', '양식', '분식', '카페', '편의점', '기타'];
+
 export const a = 1;
 //  lint 에러를 막기 위한 임시 코드
+
+/**
+ * pageList
+ * @param shop 사장님 가게 상세
+ * @param shopRegister 사장님 가게 등록
+ * @param shopDetail (shopId) 사장님 가게 상세
+ * @param shopEdit (shopId) 사장님 가게 편집
+ * @param shopNoticeRegister (shopId) 사장님 공고 등록
+ * @param shopNoticeDetail (shopId, noticeId) 사장님 공고 상세
+ * @param shopNoticeEdit (shopId, noticeId) 사장님 공고 편집
+ * @param login 로그인
+ * @param signup 회원가입
+ * @param home 일반회원 공고 리스트
+ * @param userNoticeDetail (shopId, noticeId)일반회원 공고 상세
+ * @param user 일반회원 프로필
+ * @param userEdit 일반회원 프로필 편집
+ */
+
+export const pageList = {
+  shop: () => `/shop`,
+  shopRegister: () => `/shop/register`,
+  shopDetail: (shopId: string) => `/shop/${shopId}`,
+  shopEdit: (shopId: string) => `/shop/${shopId}/edit`,
+  shopNoticeRegister: (shopId: string) => `/shop/${shopId}/register`,
+  shopNoticeDetail: (shopId: string, noticeId: string) => `/shop/${shopId}/${noticeId}`,
+  shopNoticeEdit: (shopId: string, noticeId: string) => `/shop/${shopId}/${noticeId}/edit`,
+  login: () => `/login`,
+  signup: () => `/signup`,
+  home: () => `/`,
+  userNoticeDetail: (shopId: string, noticeId: string) => `/notice/${shopId}/${noticeId}`,
+  user: () => `/user`,
+  userEdit: () => `/user/edit`,
+  page404: () => `/404`,
+  search: () => '/search'
+};
