fd8ef74dcbab495f953ffd0caf3d4ce982a18899
jinhok96 <jinhok96a@gmail.com>
2024-04-26 01:14:34 +0900

    chore: /shop/shopId 페이지 수정

--- pages/shop/[shopId]/index.tsx
+++ pages/shop/[shopId]/index.tsx
@@ -1,5 +1,4 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 import NoticeService from '@/apis/notice/Notice.service';
@@ -11,13 +10,33 @@ import MainLayout from '@/layouts/MainLayout';
 import { pageList } from '@/libs/constants/contants';
 import styles from '@/pages/shop/index.module.scss';
 
+// 토큰 나중에 쿠키로 대체
+// jwtDecode
+const token =
+  // eslint-disable-next-line max-len
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlMWFiNjk1My03MTNkLTQwNWEtOWM2NC05Njk0ZTFmZTFmOTQiLCJpYXQiOjE3MTQwNTYyOTZ9.OTO68dwg4m6AHcyHk841GlAf22OWKt5PxeTpHW_TGR4';
+
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  // token이 없으면 홈페이지로 리다이렉트
+  if (!token) {
+    return {
+      redirect: {
+        destination: pageList.home(),
+        permanent: false
+      }
+    };
+  }
+
   const { query } = context;
   const { shopId }: { shopId?: string } = query;
 
+  // shopId가 없으면 가게 상세 페이지로 리다이렉트
   if (!shopId) {
     return {
-      notFound: true
+      redirect: {
+        destination: pageList.shop(),
+        permanent: false
+      }
     };
   }
 
@@ -43,17 +62,6 @@ export default function ShopDetailPage({
   shopData: GetShopRes;
   noticeListData: GetNoticeListByShopIdRes;
 }) {
-  const router = useRouter();
-
-  // 토큰이 없으면 로그인 페이지로 리다이렉트
-  // 쿠키로 대체되면 tpyeof window === 'undefined' 제거
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('token') ?? '';
-    if (!token) {
-      router.replace(pageList.login());
-    }
-  }
-
   return (
     <>
       <section className={styles.section}>
