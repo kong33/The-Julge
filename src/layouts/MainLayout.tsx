7ec75f1b9b03e6aa84168f34e39deaca8570b867
min3eo <110177217+min3eo@users.noreply.github.com>
2024-04-26 17:38:31 +0900

    Feat: 오류 수정

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import React from 'react';
+
+import GnbData from '@/components/common/Gnb/GnbData';
+import styles from '@/layouts/layout.module.scss';
+// 헤더 푸터가 적용되어있는 레이아웃 (기본값)
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>The-Julge</title>
+      </Head>
+      <GnbData />
+      <main className={styles.mainStyle}>{children}</main>
+      {/* footer */}
+    </>
+  );
+}
