d300db24914ff387ac57a347bb1b5d84ab0df117
KimTaeEun1632 <qpwoal1324@naver.com>
2024-04-28 16:55:36 +0900

    chore: 사장님 공고상세 코드 바꿈

--- src/components/feature/CommonShopDescription/ShopDescriptionArticle.tsx
+++ src/components/feature/CommonShopDescription/ShopDescriptionArticle.tsx
@@ -1,52 +1,71 @@
+// // import Image from 'next/image';
 // import Image from 'next/image';
 
-import Button from '@/components/common/Button';
-import styles from '@/components/feature/CommonShopDescription/ShopDescriptionArticle.module.scss';
-// import shopImag from '@/public/images/defualt-shop.png';
-import { ReactComponent as LocationSvg } from '@/public/svgs/clock.svg';
+// import { useGetNotice } from '@/apis/notice/useNoticeService';
+// import Button from '@/components/common/Button';
+// import styles from '@/components/feature/CommonShopDescription/ShopDescriptionArticle.module.scss';
+// import calcFormatDuratoin from '@/libs/utils/calcFormatDuratoin';
+// // import shopImag from '@/public/images/defualt-shop.png';
+// import { ReactComponent as LocationSvg } from '@/public/svgs/clock.svg';
 
-// ShopArticle
-export function ShopDescriptionArticle() {
-  return (
-    <article className={styles.shopBox}>
-      <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>식당</h1>
-        <h2 className={styles.titleName}>도토리 식당</h2>
-      </div>
-      <div className={styles.articleShop}>
-        <div className={styles.shopImg}>
-          이미지
-          {/* <Image src={shopImag} alt="shop" width={2156} height={1232} style={{ objectFit: 'cover' }} priority /> */}
-        </div>
-        <div className={styles.shopContainer}>
-          <div className={styles.shopDetailContainer}>
-            <div className={styles.shopTitleContainer}>
-              <p className={styles.shopCategory}>시급</p>
-              <p className={styles.shopName}>15000원</p>
-            </div>
-            <div className={styles.shopLocationContainer}>
-              <LocationSvg className={styles.shopLocationIcon} />
-              <span className={styles.shopLocationText}>2023-01-02 15:00~18:00 (3시간)</span>
-            </div>
-            <p className={styles.shopDescription}>서울시 송파구</p>
-            <p>알바하기 편한 너구리네 라면집! 라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다.</p>
-          </div>
-          <div className={styles.shopButtonContainer}>
-            <Button className={styles.shopButton} size="medium" active>
-              공고 편집하기
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.noticeDetailContainer}>
-        <p className={styles.noticeDetail}>공고설명</p>
-        <p className={styles.noticeDetailText}>
-          기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가 비네요. 급해서 시급도 높였고 그렇게 바쁜
-          날이 아니라서 괜찮을거예요.
-        </p>
-      </div>
-    </article>
-  );
-}
+// // ShopArticle
+// export function ShopDescriptionArticle() {
+//   const { data } = useGetNotice('4490151c-5217-4157-b072-9c37b05bed47', '99996477-82db-4bda-aae1-4044f11d9a8b');
 
-export default ShopDescriptionArticle;
+//   const {
+//     hourlyPay,
+//     startsAt,
+//     workhour,
+//     description,
+//     closed,
+//     shop: { item: shopItem }
+//   } = data;
+
+//   const { name, address1, address2, description: shopDescription, imageUrl, originalHourlyPay } = shopItem;
+//   const workDuration = calcFormatDuratoin(startsAt, workhour);
+
+//   const handleEditNotice = () => {
+//     console.log('공고편집');
+//   };
+
+//   return (
+//     <>
+//       <article className={styles.articleNotice}>
+//         <div className={styles.noticeImg}>
+//           <Image src={imageUrl} alt="shop" width={2156} height={1232} style={{ objectFit: 'cover' }} priority />
+//         </div>
+//         <div className={styles.noticeContainer}>
+//           <div className={styles.noticeDetailContainer}>
+//             <div className={styles.noticeTitleContainer}>
+//               <p className={styles.noticeCategory}>시급</p>
+//               <div className={styles.noticeNameContainer}>
+//                 <p className={styles.noticeName}>{name}</p>
+//                 <PostTagNotice closed={closed} hourlyPay={hourlyPay} originalHourlyPay={originalHourlyPay} />
+//               </div>
+//             </div>
+//             <div className={styles.noticeInfoContainer}>
+//               <ClockSvg className={styles.noticeInfoIcon} />
+//               <span className={styles.noticeInfoText}>{workDuration}</span>
+//             </div>
+//             <div className={styles.noticeInfoContainer}>
+//               <LocationSvg className={styles.noticeInfoIcon} />
+//               <span className={styles.noticeInfoText}>{`${address1} ${address2}`}</span>
+//             </div>
+//             <p className={styles.noticeDescription}>{shopDescription}</p>
+//           </div>
+//           <div className={styles.noticeButtonContainer}>
+//             <Button className={styles.noticeButton} size="medium" active onClick={handleEditNotice}>
+//               공고편집하기
+//             </Button>
+//           </div>
+//         </div>
+//       </article>
+//       <div className={styles.noticeCommentContainer}>
+//         <p className={styles.noticeCommentTitle}>공고 설명</p>
+//         <p className={styles.noticeComment}>{description}</p>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default ShopDescriptionArticle;
