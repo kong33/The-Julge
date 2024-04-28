4bed4e92ea4ba7ad01f9e769863c21540131c362
KimTaeEun1632 <qpwoal1324@naver.com>
2024-04-28 20:03:30 +0900

    chore: 공고상세 스타일 작업중(미완성)

--- pages/taeeun.tsx
+++ pages/taeeun.tsx
@@ -1,16 +1,16 @@
-// import EmployerNotice from '@/components/Employer/EmployerNotice/EmployerNotice';
-// import AsyncBoundary from '@/components/common/AsyncBoundary';
-// import { ShopDescriptionArticle } from '@/components/feature/CommonShopDescription/ShopDescriptionArticle';
+import EmployerNotice from '@/components/Employer/EmployerNotice/EmployerNotice';
+import AsyncBoundary from '@/components/common/AsyncBoundary';
+import { ShopDescriptionArticle } from '@/components/feature/CommonShopDescription/ShopDescriptionArticle';
 
-// function TaeEun() {
-//   return (
-//     <div>
-//       <ShopDescriptionArticle />
-//       <AsyncBoundary>
-//         <EmployerNotice />
-//       </AsyncBoundary>
-//     </div>
-//   );
-// }
+function TaeEun() {
+  return (
+    <div>
+      <ShopDescriptionArticle />
+      <AsyncBoundary>
+        <EmployerNotice />
+      </AsyncBoundary>
+    </div>
+  );
+}
 
-// export default TaeEun;
+export default TaeEun;
--- src/components/feature/CommonShopDescription/ShopDescriptionArticle.tsx
+++ src/components/feature/CommonShopDescription/ShopDescriptionArticle.tsx
@@ -1,71 +1,85 @@
-// // import Image from 'next/image';
 // import Image from 'next/image';
+import classNames from 'classnames';
+import Image from 'next/image';
 
-// import { useGetNotice } from '@/apis/notice/useNoticeService';
-// import Button from '@/components/common/Button';
-// import styles from '@/components/feature/CommonShopDescription/ShopDescriptionArticle.module.scss';
-// import calcFormatDuratoin from '@/libs/utils/calcFormatDuratoin';
-// // import shopImag from '@/public/images/defualt-shop.png';
-// import { ReactComponent as LocationSvg } from '@/public/svgs/clock.svg';
+import { useGetNotice } from '@/apis/notice/useNoticeService';
+import Button from '@/components/common/Button';
+import styles from '@/components/feature/CommonShopDescription/ShopDescriptionArticle.module.scss';
+import calcFormatDuratoin from '@/libs/utils/calcFormatDuratoin';
+import { ReactComponent as ClockSvg } from '@/public/svgs/clock-notice.svg';
+import { ReactComponent as LocationSvg } from '@/public/svgs/clock.svg';
 
-// // ShopArticle
-// export function ShopDescriptionArticle() {
-//   const { data } = useGetNotice('4490151c-5217-4157-b072-9c37b05bed47', '99996477-82db-4bda-aae1-4044f11d9a8b');
+import PostTagNotice from '../Post/PostTag/PostTagNotice';
+// ShopArticle
+export function ShopDescriptionArticle() {
+  const { data } = useGetNotice('4490151c-5217-4157-b072-9c37b05bed47', '99996477-82db-4bda-aae1-4044f11d9a8b');
 
-//   const {
-//     hourlyPay,
-//     startsAt,
-//     workhour,
-//     description,
-//     closed,
-//     shop: { item: shopItem }
-//   } = data;
+  const { item: noticeItem } = data;
+  const {
+    hourlyPay,
+    startsAt,
+    workhour,
+    description,
+    closed,
+    shop: { item: shopItem }
+  } = noticeItem;
 
-//   const { name, address1, address2, description: shopDescription, imageUrl, originalHourlyPay } = shopItem;
-//   const workDuration = calcFormatDuratoin(startsAt, workhour);
+  const { name, address1, address2, description: shopDescription, imageUrl, originalHourlyPay } = shopItem;
+  const workDuration = calcFormatDuratoin(startsAt, workhour);
 
-//   const handleEditNotice = () => {
-//     console.log('공고편집');
-//   };
+  const now = new Date().getTime();
+  const startsAtDate = new Date(startsAt).getTime();
+  const isPreviousNotice = startsAtDate < now;
 
-//   return (
-//     <>
-//       <article className={styles.articleNotice}>
-//         <div className={styles.noticeImg}>
-//           <Image src={imageUrl} alt="shop" width={2156} height={1232} style={{ objectFit: 'cover' }} priority />
-//         </div>
-//         <div className={styles.noticeContainer}>
-//           <div className={styles.noticeDetailContainer}>
-//             <div className={styles.noticeTitleContainer}>
-//               <p className={styles.noticeCategory}>시급</p>
-//               <div className={styles.noticeNameContainer}>
-//                 <p className={styles.noticeName}>{name}</p>
-//                 <PostTagNotice closed={closed} hourlyPay={hourlyPay} originalHourlyPay={originalHourlyPay} />
-//               </div>
-//             </div>
-//             <div className={styles.noticeInfoContainer}>
-//               <ClockSvg className={styles.noticeInfoIcon} />
-//               <span className={styles.noticeInfoText}>{workDuration}</span>
-//             </div>
-//             <div className={styles.noticeInfoContainer}>
-//               <LocationSvg className={styles.noticeInfoIcon} />
-//               <span className={styles.noticeInfoText}>{`${address1} ${address2}`}</span>
-//             </div>
-//             <p className={styles.noticeDescription}>{shopDescription}</p>
-//           </div>
-//           <div className={styles.noticeButtonContainer}>
-//             <Button className={styles.noticeButton} size="medium" active onClick={handleEditNotice}>
-//               공고편집하기
-//             </Button>
-//           </div>
-//         </div>
-//       </article>
-//       <div className={styles.noticeCommentContainer}>
-//         <p className={styles.noticeCommentTitle}>공고 설명</p>
-//         <p className={styles.noticeComment}>{description}</p>
-//       </div>
-//     </>
-//   );
-// }
+  const handleEditNotice = () => {
+    console.log('공고편집');
+  };
 
-// export default ShopDescriptionArticle;
+  const noticeImgClosedClasses = classNames(
+    closed || startsAtDate < now ? styles.noticeImgClosed : styles.noticeImgOpened
+  );
+
+  return (
+    <article className={styles.articleContainer}>
+      <div>{name}</div>
+      <div className={styles.noticeContainer}>
+        <div className={styles.noticeImg}>
+          이미지
+          <Image src={imageUrl} alt="shop" width={2156} height={1232} style={{ objectFit: 'cover' }} priority />
+          <div className={noticeImgClosedClasses}>{isPreviousNotice ? '지난 공고' : '마감 완료'}</div>
+        </div>
+        <div className={styles.noticeDetailContainer}>
+          <div className={styles.noticeInfo}>
+            <div className={styles.noticeHourlyPayBox}>
+              <p className={styles.noticeHourlyPCategory}>시급</p>
+              <div className={styles.noticeHourlyDetail}>
+                <p className={styles.noticeName}>{hourlyPay}원</p>
+                <PostTagNotice closed={closed} hourlyPay={hourlyPay} originalHourlyPay={originalHourlyPay} />
+              </div>
+            </div>
+            <div className={styles.noticeInfoContainer}>
+              <ClockSvg className={styles.noticeInfoIcon} />
+              <span className={styles.noticeInfoText}>{workDuration}</span>
+            </div>
+            <div className={styles.noticeInfoContainer}>
+              <LocationSvg className={styles.noticeInfoIcon} />
+              <span className={styles.noticeInfoText}>{`${address1} ${address2}`}</span>
+            </div>
+            <p className={styles.noticeDescription}>{shopDescription}</p>
+          </div>
+          <div className={styles.noticeButtonContainer}>
+            <Button className={styles.noticeButton} size="medium" active onClick={handleEditNotice}>
+              공고편집하기
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.noticeCommentContainer}>
+        <p className={styles.noticeCommentTitle}>공고 설명</p>
+        <p className={styles.noticeComment}>{description}</p>
+      </div>
+    </article>
+  );
+}
+
+export default ShopDescriptionArticle;
