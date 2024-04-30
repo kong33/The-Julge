a20b9af68d8db27577a4ef390696ac95002c59db
jinhok96 <jinhok96a@gmail.com>
2024-04-30 09:37:03 +0900

    chore: 쿠키

--- pages/shop/[shopId]/register/index.tsx
+++ pages/shop/[shopId]/register/index.tsx
@@ -93,13 +93,13 @@ const formList = {
   }
 };
 
-// 토큰 나중에 쿠키로 대체
-// jwtDecode
-const token =
-  // eslint-disable-next-line max-len
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlMWFiNjk1My03MTNkLTQwNWEtOWM2NC05Njk0ZTFmZTFmOTQiLCJpYXQiOjE3MTQwNTYyOTZ9.OTO68dwg4m6AHcyHk841GlAf22OWKt5PxeTpHW_TGR4';
-
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { req } = context;
+  const { cookies } = req;
+  const { token } = cookies;
+
+  const userId = jwtDecode<{ userId: string }>(token || '').userId ?? '';
+
   // token이 없으면 홈페이지로 리다이렉트
   if (!token) {
     return {
@@ -113,7 +113,6 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
   const { query } = context;
   const { shopId: currentShopId }: { shopId?: string } = query;
 
-  const userId = jwtDecode<{ userId: string }>(token).userId ?? '';
   const { data: userData } = await UserService.getUser(userId);
   const shopId = userData?.item?.shop?.item?.id ?? '';
   const userType = userData?.item?.type ?? '';
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,5 +1,5 @@
 import { jwtDecode } from 'jwt-decode';
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -9,13 +9,13 @@ import MainLayout from '@/layouts/MainLayout';
 import { pageList } from '@/libs/constants/contants';
 import styles from '@/pages/shop/index.module.scss';
 
-// 토큰 나중에 쿠키로 대체
-// jwtDecode
-const token =
-  // eslint-disable-next-line max-len
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlMWFiNjk1My03MTNkLTQwNWEtOWM2NC05Njk0ZTFmZTFmOTQiLCJpYXQiOjE3MTQwNTYyOTZ9.OTO68dwg4m6AHcyHk841GlAf22OWKt5PxeTpHW_TGR4';
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { req } = context;
+  const { cookies } = req;
+  const { token } = cookies;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+  const userId = jwtDecode<{ userId: string }>(token || '').userId ?? '';
+
   // token이 없으면 홈페이지로 리다이렉트
   if (!token) {
     return {
@@ -26,7 +26,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     };
   }
 
-  const userId = jwtDecode<{ userId: string }>(token).userId ?? '';
   const { data: shopData } = await UserService.getUser(userId);
   const shopId = shopData?.item?.shop?.item?.id ?? '';
   const userType = shopData?.item?.type ?? '';
@@ -59,14 +58,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 // (shopId X) 사장님 가게 상세
 export default function ShopPage() {
   const router = useRouter();
-
-  // 토큰이 없으면 로그인 페이지로 리다이렉트
-  // 쿠키로 대체되면 tpyeof window === 'undefined' 제거
-  if (typeof window !== 'undefined') {
-    if (!token) {
-      router.push(pageList.login());
-    }
-  }
 
   const handleClick = {
     toShopRegisterPage: () => router.push(pageList.shopRegister())
